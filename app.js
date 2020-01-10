41272e586665977ca8cbcd645b61e12f943bc0f2
EddyGHendrickx <matthijs.hendrickx@gmail.com>
2020-01-10 12:29:01 +0100

    styling some things

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ let descDayHTML3 = document.getElementById("descDayThree");
 let descDayHTML4 = document.getElementById("descDayFour");
 let descDayHTML5 = document.getElementById("descDayFive");
 
+let daysDiv = document.getElementById("days");
 
 //function to calculate average of array (temperature in this case)
 function average(array){
@@ -20,21 +21,21 @@ function average(array){
 
 
 let button = document.getElementById("run").addEventListener("click", function () {
+    daysDiv.classList.add("bg-success");
 
     let input = document.getElementById("input");
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=antwerp&APPID=be8257068799ca9f1eccf80a6e84c26f`)
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${input.value}&APPID=be8257068799ca9f1eccf80a6e84c26f`)
         .then(response => response.json())
         .then(data => {
             console.log(data);
             //Making empty array, to push new elements in it
             let tempOfAllDays = [];
             let discOfAllDays = [];
-            let temp = data.list[0].main.temp;
             // Pushing only the temperature of every iteration in new array
             for (i = 0;i < data.list.length; i++){
                 tempOfAllDays.push(data.list[i].main.temp);
             }
-            //Pushing the discription, every 8 iterations, so I have 5 elements(for each day)
+            //Pushing the description, every 8 iterations, so I have 5 elements(for each day)
             for (o = 0;o < data.list.length; o++){
                 discOfAllDays.push(data.list[o].weather[0].description);
                 o++;
@@ -53,6 +54,7 @@ let button = document.getElementById("run").addEventListener("click", function (
             var tempDay3 = tempOfAllDays.slice(16,24);
             var tempDay4 = tempOfAllDays.slice(24,32);
             var tempDay5 = tempOfAllDays.slice(32,40);
+
             // Averaging temperature, and converting to Celcius
             var avgTempDay1 = average(tempDay1) - 273;
             var avgTempDay2 = average(tempDay2) - 273;
@@ -61,23 +63,25 @@ let button = document.getElementById("run").addEventListener("click", function (
             var avgTempDay5 = average(tempDay5) - 273;
 
             //Putting temp in HTML
-            tempDayHTML1.innerHTML = avgTempDay1.toString();
-            tempDayHTML2.innerHTML = avgTempDay2.toString();
-            tempDayHTML3.innerHTML = avgTempDay3.toString();
-            tempDayHTML4.innerHTML = avgTempDay4.toString();
-            tempDayHTML5.innerHTML = avgTempDay5.toString();
+            tempDayHTML1.innerHTML = `Today's temperature in ${input.value} is ${avgTempDay1.toString()}°C`;
+            tempDayHTML2.innerHTML = `Tomorrow's temperature in ${input.value} is ${avgTempDay2.toString()}°C`;
+            tempDayHTML3.innerHTML = `The temperature in ${input.value} 3 days from today is ${avgTempDay3.toString()}°C`;
+            tempDayHTML4.innerHTML = `The temperature in ${input.value} 4 days from today is ${avgTempDay4.toString()}°C`;
+            tempDayHTML5.innerHTML = `The temperature in ${input.value} 5 days from today is ${avgTempDay5.toString()}°C`;
 
             //Putting desc in HTML
-            descDayHTML1.innerHTML = discOfAllDays[0].toString();
-            descDayHTML2.innerHTML = discOfAllDays[1].toString();
-            descDayHTML3.innerHTML = discOfAllDays[2].toString();
-            descDayHTML4.innerHTML = discOfAllDays[3].toString();
-            descDayHTML5.innerHTML = discOfAllDays[4].toString();
+            descDayHTML1.innerHTML = `We're expecting ${discOfAllDays[0].toString()}`;
+            descDayHTML2.innerHTML = `We're expecting ${discOfAllDays[1].toString()}`;
+            descDayHTML3.innerHTML = `We're expecting ${discOfAllDays[2].toString()}`;
+            descDayHTML4.innerHTML = `We're expecting ${discOfAllDays[3].toString()}`;
+            descDayHTML5.innerHTML = `We're expecting ${discOfAllDays[4].toString()}`;
 
 
         })
         .catch(function (error) {
-            console.log("error")
+            daysDiv.classList.add("bg-danger");
+
+            daysDiv.innerHTML = "THIS DOESN'T EXIST"
         });
 
 

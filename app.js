0c466553b27a25502639b04dd636b251a9ba72f2
EddyGHendrickx <matthijs.hendrickx@gmail.com>
2020-01-10 11:26:41 +0100

    somethings working

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 // USE COMMENTS!!!!
 
+let tempDayHTML1 = document.getElementById("tempDayOne");
+let tempDayHTML2 = document.getElementById("tempDayTwo");
+let tempDayHTML3 = document.getElementById("tempDayThree");
+let tempDayHTML4 = document.getElementById("tempDayFour");
+let tempDayHTML5 = document.getElementById("tempDayFive");
+
+
+//function to calculate average of array (temperature in this case)
+function average(array){
+    return Math.floor(array.reduce((a, b) => a + b) / array.length);
+}
+
 
 let button = document.getElementById("run").addEventListener("click", function () {
 
@@ -8,16 +20,38 @@ let button = document.getElementById("run").addEventListener("click", function (
         .then(response => response.json())
         .then(data => {
             console.log(data);
+            let tempOfAllDays = [];
+            let temp = data.list[0].main.temp;
+            for (i = 0;i < data.list.length; i++){
+                tempOfAllDays.push(data.list[i].main.temp);
+            }
+            var tempDay1 = tempOfAllDays.slice(0,8);
+            var tempDay2 = tempOfAllDays.slice(8,16);
+            var tempDay3 = tempOfAllDays.slice(16,24);
+            var tempDay4 = tempOfAllDays.slice(24,32);
+            var tempDay5 = tempOfAllDays.slice(32,40);
+
+            var avgTempDay1 = average(tempDay1) - 273;
+            var avgTempDay2 = average(tempDay2) - 273;
+            var avgTempDay3 = average(tempDay3) - 273;
+            var avgTempDay4 = average(tempDay4) - 273;
+            var avgTempDay5 = average(tempDay5) - 273;
 
 
-
+            tempDayHTML1.innerHTML = avgTempDay1.toString();
+            tempDayHTML2.innerHTML = avgTempDay2.toString();
+            tempDayHTML3.innerHTML = avgTempDay3.toString();
+            tempDayHTML4.innerHTML = avgTempDay4.toString();
+            tempDayHTML5.innerHTML = avgTempDay5.toString();
 
 
 
 
 
         })
-        .catch(error => console.log("error"));
+        .catch(function (error) {
+            console.log("error")
+        });
 
 
 
